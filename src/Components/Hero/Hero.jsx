b44877dc42c99ff1b95f40269937ed7d5aadad7f
GoodButch <caranto.jaja@yahoo.com>
2023-12-12 13:25:41 +0800

    fix: hero.cssV2

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,8 +1,8 @@
 import React, { useReducer } from 'react';
-import './Hero.css';
 import hand_icon from '../Images/hand_icon.png';
 import arrow_icon from '../Images/arrow.png';
 import hero_image from '../Images/hero_image.png';
+import './Hero.css'
 
 //** */
 const actionTypes = {
